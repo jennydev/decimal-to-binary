f2a9697b8391d1586e3582bb043709175ab3408e
Zhenya Zavalova <zhenya.zavalova@scalefocus.com>
2018-07-26 18:12:24 +0300

    Small changes

--- decimal-to-binary.cs
+++ decimal-to-binary.cs
@@ -1,21 +1,21 @@
 public class Converter
 {
-    public static void Main(string args)
+    public static void Main(string[] args)
     {
         int input = int.Parse(Console.ReadLine());
-        string result = Binery(input);
+        string result = Binary(input);
         Console.WriteLine(result);
     }
 
-    private static string Binery(int number)
+    private static string Binary(int number)
     {
-        string reuslt = "";
+        string result = "";
         while (number > 0)
         {
-            binary.Append(number % 2);
+            result = (number % 2) + result;
             number /= 2;
         }
-        return binary.Length == 0 ? "0" : binary.ToString(); ;
+        return result.Length == 0 ? "0" : result;
     }
     
 }
